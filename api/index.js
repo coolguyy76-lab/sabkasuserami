61aee215ef7e97c054b7db74e4620765b039ebf2
coolguyy76-lab <coolguyy76@gmail.com>
2026-03-22 19:03:31 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,41 @@
 export default async function handler(request) {
-  return new Response("OK");
+  try {
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("id");
+
+    if (!userId) {
+      return new Response("No ID", { status: 400 });
+    }
+
+    const ACTIVE_SOURCE = "https://raw.githubusercontent.com/coolguyy76-lab/subnewww/refs/heads/main/combined.json";
+    const BLOCKED_SOURCE = "https://raw.githubusercontent.com/coolguyy76-lab/gopuy/refs/heads/main/pay.json";
+    const USERS_URL = "https://raw.githubusercontent.com/coolguyy76-lab/users/refs/heads/main/users.json";
+
+    const usersRes = await fetch(USERS_URL);
+    const users = await usersRes.json();
+
+    const user = users[userId];
+
+    const source =
+      user && user.status === "active"
+        ? ACTIVE_SOURCE
+        : BLOCKED_SOURCE;
+
+    const res = await fetch(source);
+    const text = await res.text();
+
+    return new Response(text, {
+      headers: {
+        "content-type": "application/json",
+        "profile-update-interval": "1",
+        "profile-title": "lex",
+        "subscription-auto-update-open-enable": "1",
+        "subscriptions-collapse": "0",
+        "subscriptions-expand-now": "1"
+      }
+    });
+
+  } catch (e) {
+    return new Response("error: " + e.toString(), { status: 500 });
+  }
 }
